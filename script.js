a81338c3fc69454f30cfe9ee79fe3b10389ad798
Masaaki Saitoh <masaaki.saitoh@acodebank.jp>
2022-06-03 20:38:12 +0900

    view address

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const symbol = require('/node_modules/symbol-sdk')
 
 const address = symbol.Address.createFromRawAddress("TADAGTEEVR4BF5QL367F5PVSXXFCW3SO63HSZRA")
+
 console.log("Hello Symbol")
 console.log(`Your Address : ${address.plain()}`)
+
+const dom_addr = document.getElementById('wallet-addr')
+dom_addr.innerText = address.pretty()

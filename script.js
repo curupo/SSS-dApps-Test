2d3b9356a8cc23fbad61d808ad4e5a7f61d5202d
Masaaki Saitoh <masaaki.saitoh@acodebank.jp>
2022-06-03 20:26:07 +0900

    add address

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const symbol = require('/node_modules/symbol-sdk')
 
+const address = symbol.Address.createFromRawAddress("TADAGTEEVR4BF5QL367F5PVSXXFCW3SO63HSZRA")
 console.log("Hello Symbol")
+console.log(`Your Address : ${address.plain()}`)

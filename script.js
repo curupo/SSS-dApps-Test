80ef140c34806e6f8b675a49bc356e4425e56188
Masaaki Saitoh <masaaki.saitoh@acodebank.jp>
2022-06-04 07:30:01 +0900

    input range rander

--- script.js
+++ script.js
@@ -91,3 +91,17 @@ function handleSSS() {
     transactionHttp.announce(signedTx)
   })
 }
+
+// input range rander
+function render (output, value) {
+  output.innerHTML = value
+}
+
+const input = document.querySelector('input')
+const output = document.querySelector('b')
+
+input.addEventListener('input', function (e) {
+  render(output, e.target.value)
+}, false)
+
+render(output, input.value)
